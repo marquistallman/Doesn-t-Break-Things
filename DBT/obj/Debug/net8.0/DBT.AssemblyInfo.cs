44f7c8449b62ad087975dfa3b261cc68bf1b8e1d
marquistallman <daviricanun@gmail.com>
2025-12-23 11:15:22 -0500

    Fist class for solving bugs

--- DBT/obj/Debug/net8.0/DBT.AssemblyInfo.cs
+++ DBT/obj/Debug/net8.0/DBT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DBT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c53b4220437bbe999aca716e8d7b0ecb0fbcb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c32b5dacde36de53f5bd3e162e011c1d6908683b")]
 [assembly: System.Reflection.AssemblyProductAttribute("DBT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DBT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
